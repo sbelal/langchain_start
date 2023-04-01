6369b58089dee4da08b02be9bda5308a8e24a247
Sal Belal <sbelal@gmail.com>
2023-04-01 04:10:44 +0100

    lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 import { OpenAI } from "langchain";
 
+dotenv.config();
 
-dotenv.config()
+console.log("Hello World!");
+console.log(process.env.OPENAI_API_KEY);
 
-
-console.log("Hello World!")
-console.log(process.env.OPENAI_API_KEY)
-
-
-const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9 });
+const model = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0.9,
+});
 
 const res = await model.call(
-    "What would be a good company name a company that makes colorful socks?"
+  "What would be a good company name a company that makes colorful socks?"
 );
 console.log(res);
